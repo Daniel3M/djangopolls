f6e0a27d87d3329166f49a223f202d8e4a3431ac
Daniel3M <daniel.muhammad96@gmail.com>
2021-05-03 12:01:26 +0700

    bagian 5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -17,7 +17,7 @@ def results(request, question_id):
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
-
+    
     def get_queryset(self):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
@@ -26,6 +26,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
